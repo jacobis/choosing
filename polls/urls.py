200e809e764d5e125d8d95af570914ec6e26900d
jacobis <remind.jacob@gmail.com>
2016-11-18 16:15:14 +0900

    Add teaming process

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
